5d07aeba1ae905dbced3442b7206c8ea51c17e0e
angel3235 <152671354+yapakovasabina@users.noreply.github.com>
2024-03-01 01:45:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-// Устанавливаем HTTP статус код 301 Moved Permanently
-header("HTTP/1.1 301 Moved Permanently");
-// Устанавливаем заголовок Location для перенаправления
-header("Location: http://example.com/new-page.php");
-exit(); // Завершаем выполнение скрипта
+// Выводит "Привет, мир!" на экран
+echo "Привет, мир!";
 ?>
